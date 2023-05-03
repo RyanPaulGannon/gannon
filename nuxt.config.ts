97cb16d2c070fadf525b5f958cc2cbd69dd50961
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-05-03 17:33:44 +0100

    Add uno, update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon"],
-  colorMode: {
-    classSuffix: "",
-  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'nuxt-icon'],
 })
