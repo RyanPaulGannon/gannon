b65dc1a047c69a49d1a62cc2ba3766be5292d9f8
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-12-31 12:40:24 +0000

    `App`: Update main component, add button

---
+++ src/hire.rs
@@ -0,0 +1,8 @@
+use leptos::*;
+
+#[component]
+pub fn Hire() -> impl IntoView {
+    view! {
+        <button>"Hire Me?"</button>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,40 @@
 use leptos::*;
 
+mod hire;
+use hire::*;
+
 turf::style_sheet!("src/main.scss");
+
 fn main() {
-    leptos::mount_to_body(|| view! { <App/> })
-}
+    mount_to_body(|| {
+        view! {
+            <style>{STYLE_SHEET}</style>
+            <div id="main">
+                <h1>"Ryan Paul Gannon"</h1>
 
-#[component]
-fn App() -> impl IntoView {
-    view! {
-        <style>{STYLE_SHEET}</style>
-        <div id="main">
-            <h1>"Ryan Paul Gannon"</h1>
+                <Hire />
+                <div class="card">
+                    <p>
+                    "Bonjour, I'm Ryan, enchanté! I'm a Full Stack Developer from
+                    🏴󠁧󠁢󠁥󠁮󠁧󠁿 Manchester, UK"
+                    <br />
+                    "Currently working at "
+                        <a href="https://ekatree.com" target="_blank">Ekatree</a>.
+                    </p>
 
-            <div class="card">
-                <p>
-                "Bonjour, I'm Ryan, enchanté! I'm a Full Stack Developer from
-                🏴󠁧󠁢󠁥󠁮󠁧󠁿 Manchester, UK"
-                <br />
-                "Currently working at "
-                    <a href="https://ekatree.com" target="_blank">Ekatree</a>.
-                </p>
+                    <p id="main-text">
+                    "You can find me contributing to Open Source on "
+                        <a href="https://github.com/ryanpaulgannon" target="_blank">GitHub</a>
+                    " (with a particular interest in memory management "
+                    "and Rust), and working on some personal projects ou apprendre le francais."
+                    </p>
 
-                <p id="main-text">
-                "You can find me contributing to Open Source on "
-                    <a href="https://github.com/ryanpaulgannon" target="_blank">GitHub</a>
-                " (with a particular interest in memory management "
-                "and Rust), and working on some personal projects ou apprendre le francais."
-                </p>
-
-                <p>
-                "I've a keen sporting interest, mostly for ⚽️, 🏏 and the NFL 🏈. I'll talk almost anything sport!"
-                </p>
-                // <p>"Some projects I'm working on/contributing to:"</p>
+                    <p>
+                    "I've a keen sporting interest, mostly for ⚽️, 🏏 and the NFL 🏈. I'll talk almost anything sport!"
+                    </p>
+                    // <p>"Some projects I'm working on/contributing to:"</p>
+                </div>
             </div>
-        </div>
-    }
+        }
+    })
 }
