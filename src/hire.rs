84019f80c9719356e743f5df130be1d414291ce4
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-12-31 16:03:28 +0000

    `Main`: Added functional button

--- src/hire.rs
+++ src/hire.rs
@@ -2,7 +2,11 @@ use leptos::*;
 
 #[component]
 pub fn Hire() -> impl IntoView {
+    let (text, show_text) = create_signal("".to_string());
     view! {
-        <button>"Hire Me?"</button>
+        <button on:click=move |_| show_text.set("ryan@gannon.one".to_string())>"Hire Me?"</button>
+            <p>
+                <a href="mailto:ryan@gannon.one">{ text }</a>
+            </p>
     }
 }
